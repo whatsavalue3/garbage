6a3109201944f04cc8ec3e485b6266a7ef4cfa6d
wav3 <reltwhy@gmail.com>
2024-11-07 13:17:12 +0100

    Finally C does work

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include "base.h"
 
-void RAWCODE main() {
+ATTR((section(".main"), naked)) void main() {
 	__asm__(
-		"MOV 0xA, %%AH\n"
-		"MOV 'K', %%AL\n"
-		"MOV 1, %%CX\n"
-		"MOV 2, %%BL\n"
+		"MOV $0xA, %%AH\n"
+		"MOV $'K', %%AL\n"
+		"MOV $1, %%CX\n"
+		"MOV $2, %%BL\n"
 		"INT $0x10\n"
-		"halt:\n"
-		"JMP halt\n"
 		:
 		:
 	);
+	
+	while(1)
+	{
+		
+	}
 }
